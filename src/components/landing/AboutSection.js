4467ab530bb657a3d368b7c4d44834809adb17b1
diligentAIprojects <diligent.ai.work@gmail.com>
2025-02-02 13:14:58 +0000

    about section

--- src/components/landing/AboutSection.js
+++ src/components/landing/AboutSection.js
@@ -1,7 +1,13 @@
+import { Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
 
 function AboutSection() {
-  return <div>AboutSection</div>;
+  return (
+    <Flex minH="50vh" flexDir="column">
+      <Text my="20">We are cool</Text>
+      <Image h="400px" src="./assets/images/community_bg.png" />
+    </Flex>
+  );
 }
 
 export default AboutSection;
