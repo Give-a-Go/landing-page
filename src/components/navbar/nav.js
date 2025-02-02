0104c045752233bf607ba59f2cd6c0922f2af53e
saujix <chutkipasbola@gmail.com>
2025-02-02 12:44:15 +0000

    navbar

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -2,26 +2,58 @@ import { Flex } from "@chakra-ui/react";
 
 export default function Navbar(){
     return <>
-        <Flex
-        width = "100vw"
-        background="red"
-        padding="10px"
+    <Flex
+    width="100vw"
+    justifyContent="center"
+    paddingTop="20px">
+
+    <Flex
+        width = "80%"
+        padding="20px"
         height="50px"
-        color="black"
+        color="white"
         justifyContent="space-between"
+        borderRadius="20px"
+        alignItems="center"
+        border="1px solid white"
         >
             <div className="logo">
-                HI
+                LOGO
             </div>
             <Flex className="links"
+            justifyContent="space-between"
+            gap="20px"
             >
-                <div>a</div>
-                <div>a</div>
-                <div>a</div>
-                <div>a</div>
-                <div>a</div>
+                <Flex
+                border="1px solid"
+                // background="green"
+                padding="5px"
+                borderRadius="5px"
+                >
+                    Sign Up
+                </Flex>
+                <Flex
+                border="1px solid"
+                // background="red"
+                padding="5px"
+                borderRadius="5px"
+                >
+                    Log In
+                </Flex>
+
+                <Flex
+                border="1px solid"
+                // background="red"
+                padding="5px"
+                borderRadius="5px"
+                >
+                    Blog
+                </Flex>
             </Flex>
 
         </Flex>
+
+    </Flex>
+        
     </>
 }
